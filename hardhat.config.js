ac5c54f73ecf4ddb4e986622ba81a2a42b723e5c
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2022-12-02 14:31:46 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require ("@nomiclabs/hardhat-waffle");
 
 
-const ALCHEMY_API_KEY = "zdj7lIBtAEYKFMzMxZccG2mZ8-FtvdUf";
-const Private_Key = "0x0a059f43211fa8bc7b35b68dafc733c0ab23553c543078d484c5f456c920e656";
+const ALCHEMY_API_KEY = "alchemy api url";
+const Private_Key = "your key";
 import('hardhat/config').HardhatUserConfig ;
 module.exports = {
   solidity: "0.8.9",
